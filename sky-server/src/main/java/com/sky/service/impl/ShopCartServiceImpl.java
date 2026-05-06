1f534c4185db8b98460af23ccce590c6b7df65b6
Borzng <3407268318@QQ.com>
2026-05-06 13:09:09 +0800

    查看购物车

--- sky-server/src/main/java/com/sky/service/impl/ShopCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShopCartServiceImpl.java
@@ -73,4 +73,20 @@ public class ShopCartServiceImpl implements ShoppingCartService  {
            shoppingCartMapper.insert(shoppingCart);
         }
     }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        //获取当前微信用户id
+        Long userId=BaseContext.getCurrentId();
+       ShoppingCart shoppingCart= ShoppingCart.builder()
+                       .userId(userId)
+                               .build();
+
+        List<ShoppingCart>list = shoppingCartMapper.list(shoppingCart );
+        return list;
+    }
 }
