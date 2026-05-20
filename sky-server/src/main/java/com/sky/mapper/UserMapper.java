2b42760cb2c1565c3bb42c1b1c55cf98eb055ff0
Borzng <3407268318@QQ.com>
2026-05-20 21:44:57 +0800

    用户统计代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 
 public interface UserMapper {
@@ -21,4 +23,11 @@ public interface UserMapper {
     void insert(User user);
 @Select("select * from user where id=#{userId}")
     User getById(Long userId);
+
+    /**
+     * 统计用户数据
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
