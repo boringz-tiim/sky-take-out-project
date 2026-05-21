67884cfcd20472e9250b2a8a033f5e4029842da1
Borzng <3407268318@QQ.com>
2026-05-21 20:46:34 +0800

    销量前十统计

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,15 +1,21 @@
 package com.sky.service.impl;
 
+import ch.qos.logback.core.rolling.helper.IntegerTokenConverter;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.ApiOperation;
+
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -17,6 +23,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 
@@ -94,6 +101,77 @@ public class ReportServiceImpl implements ReportService {
                 .newUserList(StringUtils.join(newUserList,","))
                 .build();
 
+    }
+
+    /**
+     * 根据条件统计订单数量
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end) {
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while(!begin.equals(end)){
+            begin=begin.plusDays(1);
+            dateList.add(begin);
+        }
+        //存放每天的订单总数
+        List<Integer> orderCountList=new ArrayList<>();
+        //存放每天的有效订单数
+        List<Integer> validOrderCountList=new ArrayList<>();
+        //遍历dateList集合，查询每天的有效订单数和订单总数
+        for(LocalDate date:dateList){
+            LocalDateTime beginTime=LocalDateTime.of(date,LocalTime.MIN);
+            LocalDateTime endTime=LocalDateTime.of(date,LocalTime.MAX);
+            Integer orderCount=getOrderCount(beginTime,endTime,null);
+            Integer validOrderCount=getOrderCount(beginTime,endTime,Orders.COMPLETED);
+
+            orderCountList.add(orderCount);
+            validOrderCountList.add(validOrderCount);
+        }
+        Integer totalOrderCount=orderCountList.stream().reduce(Integer::sum).get();
+        Integer validOrderCount=validOrderCountList.stream().reduce(Integer::sum).get();
+        Double orderCompletion=0.0;
+        if(totalOrderCount!=0){
+            orderCompletion=validOrderCount.doubleValue()/totalOrderCount;
+        }
+        return OrderReportVO.builder()
+                .dateList(StringUtils.join(dateList,","))
+                .orderCountList(StringUtils.join(orderCountList,","))
+                .validOrderCountList(StringUtils.join(validOrderCountList,","))
+                .totalOrderCount(totalOrderCount)
+                .validOrderCount(validOrderCount)
+                .orderCompletionRate(orderCompletion)
+                .build();
+
+    }
+
+    /**
+     * 统计制定时间区间内的销量排名前10
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end) {
+        LocalDateTime beginTime=LocalDateTime.of(begin,LocalTime.MIN);
+        LocalDateTime endTime= LocalDateTime.of(end,LocalTime.MAX);
+        List<GoodsSalesDTO> salesTop10=orderMapper.getSalesTop10(beginTime,endTime);
+        List<String> names=salesTop10.stream().map(GoodsSalesDTO::getName).collect(Collectors.toList());
+        String nameList=StringUtils.join(names,",");
+        List<Integer> numbers=salesTop10.stream().map(GoodsSalesDTO::getNumber).collect(Collectors.toList());
+        String numberList=StringUtils.join(numbers,",");
+        return SalesTop10ReportVO.builder().nameList(nameList).numberList(numberList).build();
+    }
+
+    private Integer getOrderCount(LocalDateTime begin, LocalDateTime end, Integer status) {
+        Map map=new HashMap();
+        map.put("begin",begin);
+        map.put("end",end);
+        map.put("status",status);
+        return orderMapper.countByMap(map);
     }
 
 
